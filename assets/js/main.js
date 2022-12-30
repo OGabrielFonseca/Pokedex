6b3308d40970f9886e5dd820465d18c4a3e06057
GenieFlick <gabriel1911260@gmail.com>
2022-12-30 20:28:26 -0300

    make background color dinamic

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 function convertPokemonToHtml (pokemon)
 {
     return `
-    <li class="pokedex__pokemon-card">
+    <li class="pokedex__pokemon-card ${pokemon.type}">
         <span class="pokemon-card__number">
             #0${pokemon.number}
         </span>
@@ -12,7 +12,7 @@ function convertPokemonToHtml (pokemon)
         </span>
         <div class="pokemon-card__description">
             <ol class="pokemon-card__types">
-                ${pokemon.types.map ((type) => `<li class="pokemon-card__type">${type}</li>`).join(" ")}
+                ${pokemon.types.map ((type) => `<li class="pokemon-card__type  ${pokemon.type}">${type}</li>`).join(" ")}
             </ol>                    
             <img class= "pokemon-card__img" 
             src="${pokemon.image}"
